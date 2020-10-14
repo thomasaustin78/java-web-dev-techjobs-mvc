ab4953cfe417415b939f1d4c65dfc34a49acf47a
Austin Thomas <thomasaustin78@gmail.com>
2020-10-14 18:44:27 -0500

    finished the search controller and displayed all data in a table

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -1,11 +1,14 @@
 package org.launchcode.javawebdevtechjobsmvc.controllers;
 
+import jdk.jfr.Event;
 import org.launchcode.javawebdevtechjobsmvc.models.Job;
 import org.launchcode.javawebdevtechjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.engine.AttributeName;
 
+import javax.management.Attribute;
 import java.util.ArrayList;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
@@ -25,4 +28,19 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+@RequestMapping(value = {"results"},method = RequestMethod.POST)
+    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
+        ArrayList<Job>jobs;
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+            model.addAttribute("title","All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType,searchTerm);
+            model.addAttribute("title","Jobs with " + columnChoices.get(searchType));
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        return "search";
+
+}
 }
